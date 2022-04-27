f75522225e05e423dfd830203fa172ad81c2c5ed
Arial Hoxhaj <arialhoxhaj@Arial-Atis-Macbook.local>
2022-04-27 17:27:08 +0200

    mrroklla

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,26 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Miresevini tek Merrnjoftime</title>
         <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5340716281741826"
+          crossorigin="anonymous"
+        ></script>
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Miresevini!" />
+        <p className="description">Per me shume informacion kliko me poshte.</p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
